e647b83f3ca808e893af62a41e531344d83e7c10
Kalyan Mazumder <kalyaninform@gmail.com>
2020-10-08 11:45:03 +0530

    initial commit

---
+++ src/main/java/com/techdenovo/app/hellospringsecurity/HellospringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.techdenovo.app.hellospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techdenovo/app/hellospringsecurity/HellospringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdenovo.app.hellospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
